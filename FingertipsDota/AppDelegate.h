c7d5c3ad3148f47080e9eb4022ebc4ada338a2d0
xujingbao <xujingbao@gmail.com>
2012-08-22 23:11:33 +0800

    create

---
+++ FingertipsDota/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FingertipsDota
+//
+//  Created by xujingbao on 12-8-22.
+//  Copyright (c) 2012年 xujingbao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FingertipsDota/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FingertipsDota
+//
+//  Created by xujingbao on 12-8-22.
+//  Copyright (c) 2012年 xujingbao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
